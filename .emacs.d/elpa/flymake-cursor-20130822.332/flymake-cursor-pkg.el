f173e7b4ddd2e4372ae840a62678e70959a2bc29
tpanum <thomas@panum.dk>
2018-05-25 10:05:11 +0200

    Removed elpa

--- .emacs.d/elpa/flymake-cursor-20130822.332/flymake-cursor-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-cursor" "20130822.332" "displays flymake error msg in minibuffer after delay" 'nil :url "http://www.emacswiki.org/emacs/flymake-cursor.el" :keywords '("languages" "mode" "flymake"))
--- .emacs.d/elpa/vue-html-mode-20180104.1421/vue-html-mode-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; vue-html-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "vue-html-mode" "vue-html-mode.el" (23134 24716
-;;;;;;  430127 145000))
-;;; Generated autoloads from vue-html-mode.el
-
-(autoload 'vue-html-mode "vue-html-mode" "\
-Major mode for Vue.js templates.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; vue-html-mode-autoloads.el ends here
