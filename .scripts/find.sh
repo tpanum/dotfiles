182f4ae0714e792c24fa9da25b4e049e86922df2
Thomas Kobber Panum <thomas@panum.dk>
2018-09-14 12:49:23 +0200

    Migrated to alacritty

--- .scripts/find.sh
+++ .scripts/find.sh
@@ -2,11 +2,14 @@
 IFS=':'
 
 get_selection() {
-    find ~ -type d ! -readable -prune -o -print | fzf --reverse --color pointer:3,info:14,bg+:16
+    find ~ -type d ! -readable -prune -o -print | fzf --reverse --color pointer:3,info:14,bg+:-1
 }
 
 if selection=$( get_selection ); then
+    echo "Meow"
     nohup mimeo "$selection" >/dev/null 2>&1 &
     sleep 0.05
 fi
 
+echo -ne ' '
+exit 0
